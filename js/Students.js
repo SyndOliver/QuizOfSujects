df170db53ad21bcdb5d2d4f263bbfeaa59718ff8
ha <some@email.com>
2021-04-20 21:55:33 +0700

    first commit

---
+++ js/Students.js
@@ -0,0 +1,23 @@
+﻿var listsv = [
+    {
+        "username": "minhphuc",
+        "password": "123456",
+        "fullname": "Trần Minh Phúc",
+        "email": "phuctmps13568@fpt.edu.vn",
+       
+    },
+    {
+        "username": "pheonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "email": "pheonv@fpt.edu.vn",
+       
+       
+    },
+    {
+        "username": "nopt",
+        "password": "iloveyou",
+        "fullname": "Phạm Thị Nở",
+        "email": "nopt@fpt.edu.vn",
+    }
+]
